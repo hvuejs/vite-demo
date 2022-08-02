fa7e13a98968f3e57e88e1e8e3c793c377f390ca
lijinghao <wwwvipljh@163.com>
2022-08-02 10:32:14 +0800

    :hammer: 组件库代码迁移到vue分支.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vite-demo 
+# happy-vue
 
 > 学习开发vue3的ui库
 
--- vite-react/src/shims-react.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
-declare module 'react-copy-to-clipboard';
-declare module "styled-components";
-declare module "react-router-config";
